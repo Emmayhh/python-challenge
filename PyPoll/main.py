d7865a37c0401da6969f827ff93869af5b8fe6af
Han Yu <120348806+Emmayhh@users.noreply.github.com>
2023-01-18 01:14:58 -0500

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,7 +4,7 @@ import csv
 TotalVotesCast = 0
 candidates_list = []
 candidates_dic = {}
-PercentageVotes = [0,0,0]
+PercentageVotes = []
 candidatesW = []
 
 winners = ""
@@ -12,7 +12,7 @@ winnerNum = 0
 
 
 #Create path to csv file
-election_csv = os.path.join("/Users/hany/Desktop/Bootcamp/03-Python/Ass3/python-challenge/PyPoll/Resources/election_data.csv")
+election_csv = os.path.join("Resources/election_data.csv")
 
 with open(election_csv, 'r') as csvfile:
 
@@ -27,8 +27,9 @@ with open(election_csv, 'r') as csvfile:
         TotalVotesCast += 1
 
         candidates = (row[2])
-#option = list, votes = dic
+
     # if not in in the current candidates
+    #A complete list of candidates who received votes
         if candidates not in candidates_list:
             #add it to list
             candidates_list.append(candidates)
@@ -37,15 +38,12 @@ with open(election_csv, 'r') as csvfile:
         
         candidates_dic[candidates] += 1
 
-    #A complete list of candidates who received votes
+    
 
 print("------------------------------------------------")
 print("Financial Analysis")
 print("------------------------------------------------")
 print(f"Total votes: {TotalVotesCast}")
-
-#dic opinion =list 
-#dic votes = dic
 
 for candidatesW in candidates_dic:
 
@@ -59,11 +57,6 @@ for candidatesW in candidates_dic:
     if (winnerNum < VotesCast):
         winnersName = candidatesW
     
-    # calculate the percentage of votes per candicate (3 decimal points)
-
-#print(candidatesW)
-
-#printing the results
 
 for item in candidates_dic:
     print(f"{item}: {PercentageVotes:.3f}% ({candidates_dic[item]})")
@@ -72,7 +65,8 @@ print("------------------------------------------------")
 print(f"Winner:{winnersName}")
 print("------------------------------------------------")   
 
-text_file = ("/Users/hany/Desktop/Bootcamp/03-Python/Ass3/python-challenge/PyPoll/analysis/PyPoll_text_file.txt")
+# calculate the percentage of votes per candicate (3 decimal points)
+text_file = ("analysis/PyPoll_text_file.txt")
 
 with open(text_file, "w") as textfile:
     textfile.write("------------------------------------------------\n")
